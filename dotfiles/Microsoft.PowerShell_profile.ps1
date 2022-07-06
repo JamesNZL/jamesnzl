69a68eb1e66eaca85d903403ed074c9a39be3331
James <james@jamesnzl.xyz>
2022-07-07 00:33:23 +1200

    feat(dotfiles): :technologist: add ssh autocomplete
    sourced from https://gist.github.com/backerman/2c91d31d7a805460f93fe10bdfa0ffb0?permalink_comment_id=4130019#gistcomment-4130019

--- dotfiles/Microsoft.PowerShell_profile.ps1
+++ dotfiles/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,47 @@
+using namespace System.Management.Automation
+
+### SSH autocompletion
+Function Get-Hosts($configFile) {
+    Get-Content $configFile `
+    | Select-String -Pattern "^Host " `
+    | ForEach-Object { $_ -replace "host ", "" } `
+    | Sort-Object -Unique `
+}
+
+Register-ArgumentCompleter -CommandName ssh, scp, sftp -Native -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+
+    $sshDir = "${Env:HOMEPATH}\.ssh"
+
+    $hosts = Get-Content "$sshDir\config" `
+    | Select-String -Pattern "^Include " `
+    | ForEach-Object { $_ -replace "include ", "" } `
+    | ForEach-Object { Get-Hosts "$sshDir/$_" } `
+
+    $hosts += Get-Hosts "$sshDir\config"
+    $hosts = $hosts | Sort-Object -Unique
+
+    # For now just assume it's a hostname.
+    $textToComplete = $wordToComplete
+    $generateCompletionText = {
+        param($x)
+        $x
+    }
+    if ($wordToComplete -match "^(?<user>[-\w/\\]+)@(?<host>[-.\w]+)$") {
+        $textToComplete = $Matches["host"]
+        $generateCompletionText = {
+            param($hostname)
+            $Matches["user"] + "@" + $hostname
+        }
+    }
+
+    $hosts `
+    | Where-Object { $_ -like "${textToComplete}*" } `
+    | ForEach-Object { [CompletionResult]::new((&$generateCompletionText($_)), $_, [CompletionResultType]::ParameterValue, $_) }
+}
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-Expression
+Import-Module -Name Terminal-Icons
+
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
